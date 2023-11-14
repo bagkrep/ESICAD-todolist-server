0d9fc37c83b75f1468e4ef5c9d37fcda400a6a11
Aurélien VERNAY <aurevernay@yahoo.fr>
2023-11-14 11:59:37 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import express from "express";
+
+const todos = [
+  {
+    todoId: 1,
+    text: "fetch loaded data from vue frontend app ",
+    done: true,
+  },
+  {
+    todoId: 2,
+    text: "display those todos inside my app",
+    done: true,
+  },
+  {
+    todoId: 3,
+    text: "develop methods to update my json db of todos",
+    done: false,
+  },
+  {
+    todoId: 4,
+    text: "watch mutation of mesTodos to automatically update todos",
+    done: false,
+  },
+];
+
+// on créé une instance d'une application Express
+const app = express();
+
+// on précise à l'application qu'elle doit parser le body des requêtes en JSON (utilisation d'un middleware)
+app.use(express.json());
+
+// on peut utiliser app.get, app.post, app.put, app.delete, etc.. ()
+
+// on définit une route GET /todos, et la fonction qui s'exécute lorsque le serveur reçoit une requête qui matche
+app.get("/todos", (request: express.Request, result: express.Response) => {
+  console.log(`route "/todos" called`);
+  return result.status(200).json(todos);
+});
+
+// une autre route pour récupérer 1 TODO
+app.get("/todos/:id", (request: express.Request, result: express.Response) => {
+  console.log(`route "/todos/:id" called`);
+  console.log(`params of the request : ${JSON.stringify(request.params)}`);
+  return result
+    .status(200)
+    .json(
+      todos.find((todo) => todo.todoId === Number(request.params.id)) || null,
+    );
+});
+
+app.listen(8080, () => console.log("server started, listening on port 8080"));
